3dfef968af15a54764bf0e347264d66e6a47a380
Emraldis <alfie.feltham@gmail.com>
2016-06-22 21:59:09 -0700

    test

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,8 +7,14 @@ package test;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Alfie Feltham
@@ -22,12 +28,25 @@ public class Test {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Setting up pins");
         final GpioController gpio = GpioFactory.getInstance();
-        System.out.println("Light turning on");
-        final GpioPinDigitalOutput ledOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "LED", PinState.HIGH);
-        ledOne.setShutdownOptions(true,PinState.LOW);
-        Thread.sleep(1000);
-        System.out.println("Light turning off");
-        ledOne.low();
+        final GpioPinDigitalInput button = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, PinPullResistance.PULL_DOWN);
+        button.addListener(new GpioPinListenerDigital(){
+
+            @Override
+            public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event) {
+                System.out.println("Light turning on");
+                final GpioPinDigitalOutput ledOne = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "LED", PinState.HIGH);
+                ledOne.setShutdownOptions(true,PinState.LOW);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                System.out.println("Light turning off");
+                ledOne.low();
+                
+            }
+            
+        });
     }
     
 }
