92bb5e1b9f61859c9df2228315805199f8c51bc9
Emraldis <alfie.feltham@gmail.com>
2016-07-06 12:47:16 -0700

    test

--- SensorTestCode/src/GPIO_Manager/LogicRelay.java
+++ SensorTestCode/src/GPIO_Manager/LogicRelay.java
@@ -46,6 +46,11 @@ public class LogicRelay {
         String input;
         String tempString = " ";
         System.out.println("\nSYSTEM - Beginning Induction, " + (sysData.inductionTime / 100) + " seconds remaining.");
+        if((i < (inductionTime + respondTime)) && (!sysData.buttonControl)){
+            System.out.println("\nYES");
+        }else {
+            System.out.println("\nNO");
+        }
         while((i < (inductionTime + respondTime)) && (!sysData.buttonControl)){
         /*-----------------------|Code for getting temp data from file|-----------------------*/
             try {
